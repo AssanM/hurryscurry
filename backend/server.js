4489ec015d1b789bb6b11305197143514b318010
root <root@cv4572159.novalocal>
2025-06-14 19:13:05 +0300

    Account Data 10

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,12 @@ import promotionRouter from './routes/promotionRoute.js'
 
 
 // App Config
+<<<<<<< Updated upstream
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ extended: true, limit: '10mb' }))
+=======
+const app = express()
+>>>>>>> Stashed changes
 const port = process.env.PORT || 4001
 
 connectDB()
